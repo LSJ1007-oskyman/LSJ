d85f1037ea30ab973f3db0294063b879f8333cce
user <user@a>
2024-07-01 16:03:45 +0900

    7/1

--- javaproject/src/p2024_07_01/Test.java
+++ javaproject/src/p2024_07_01/Test.java
@@ -1,11 +1,13 @@
 package p2024_07_01;
 
-public class Test {
-//접근제어자    사용자정의 클래스
+public    class Test {
+//접근제어자       사용자정의 클래스
 	
-
-	public static void main(String[] args) {
-		
+	//main() 메소드는 자바가상머신(JVM,java.exe)이 가장 먼저 호출하는 메소드이다.
+	//1개 클래스가 독립적으로 실행되기 위해서는 반드시 main메서드가 필요하다.
+	//main메서드는 다른클래스에서 임의로 호출이 불가능하다
+	public    static    void main(String[] args) {
+//  접근제어자   정적메서드        main메소드
 
 		System.out.println("자바출력성공");
 
