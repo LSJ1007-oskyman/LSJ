9f90af569e4c242c563916cda902c33acd45880c
user <user@a>
2024-07-11 17:02:29 +0900

    7/11

--- javaproject/src/p2024_07_11/StringTest01.java
+++ javaproject/src/p2024_07_11/StringTest01.java
@@ -8,7 +8,7 @@ public class StringTest01 {
 		
 		str1.toUpperCase();
 		System.out.println(str1);
-		System.out.println(str1.toUpperCase());
+		System.out.println(str1.toUpperCase());//->str1.toUpperCase()는 힙메모리에 저장되나 접근가능한 주소값을 가진 변수가 없다 ->이에 Garbage Collection가 지워줌
 		
 		String str2 = str1.toUpperCase();
 		System.out.println(str2);
