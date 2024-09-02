3429452067b0bd688203a40f76cbed38b41797d6
LSJ1007-oskyman <pianoman1007@gmail.com>
2024-09-02 16:06:27 +0900

    Create App.js

---
+++ web/src/main/webapp/REACT/실습/2024_09_01/compoent03_스프레드연산자+length/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './component/Header';
+import Main from './component/Main';
+import Footer from './component/Footer';
+
+//스프레드 연산자로 여러개의 값 전달하기
+function App() {
+  const mainProps = {
+    name : '홍길동',
+    location : '서울시',
+    favorList : ['파스타','빵','떡볶기']
+  }
+  return (
+    <div className="App">
+      <Header/>
+      <Main {...mainProps}/>   {/* 스프레드연산자 */}
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
