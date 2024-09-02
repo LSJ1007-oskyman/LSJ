607e075d35a1c3a9ac7bc1a93f27b5735fce3f4c
LSJ1007-oskyman <pianoman1007@gmail.com>
2024-09-02 15:30:25 +0900

    Create App.js

---
+++ web/src/main/webapp/REACT/실습/2024_09_01/component02/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './component/Header';
+import Main from './component/Main';
+import Footer from './component/Footer';
+
+function App() {
+  return (
+    <div className='App'>
+          <Header/>
+          <Main name="홍길동" color="blue"/>
+          <Footer/>
+    </div>
+  );
+}
+
+export default App;
