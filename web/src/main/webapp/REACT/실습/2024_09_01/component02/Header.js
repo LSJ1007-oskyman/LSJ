ef6c00a9ec860378200dc5a12654dcaffd2c7faa
LSJ1007-oskyman <pianoman1007@gmail.com>
2024-09-02 15:29:24 +0900

    Create Header.js

---
+++ web/src/main/webapp/REACT/실습/2024_09_01/component02/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header=(props)=>{
+    return(
+        <div>
+            <header>
+                <h1>헤드입니다.</h1>
+            </header>
+            <hr/>
+        </div>
+    );
+}
+
+export default Header;
