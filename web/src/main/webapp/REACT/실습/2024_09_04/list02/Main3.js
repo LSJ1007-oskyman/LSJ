c28c42e775d59316c8c068f7bfc4b20a412a95ab
LSJ1007-oskyman <pianoman1007@gmail.com>
2024-09-04 14:37:08 +0900

    Create Main3.js

---
+++ web/src/main/webapp/REACT/실습/2024_09_04/list02/Main3.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main3=(props)=>{
+    return(
+        <div>
+            <h3>안녕 하세요?{props.name}({props.age}세)입니다.</h3>
+        </div>
+    );
+}
+
+export default Main3;
