7b8a78b2569794ccf182c89b72d0c25788b830c1
LSJ1007-oskyman <pianoman1007@gmail.com>
2024-09-04 12:39:04 +0900

    Update Main.js

--- web/src/main/webapp/REACT/실습/2024_09_04/list01/Main.js
+++ web/src/main/webapp/REACT/실습/2024_09_04/list01/Main.js
@@ -1,8 +1,31 @@
 import React from "react";
 
 const Main=(props)=>{
+    const numbers = [1,3,5];
+    const subject = ['자바','오라클','JSP','Spring','파이썬'];
+    
     return(
-        
+        <ul>
+            {numbers.map((number,idx)=>{
+                return <li>{number}</li>
+                })
+
+            }
+            <br/>
+
+            {subject.map((name)=>{
+                return <li >{name}</li>
+            })}<br/>       
+
+            {subject.map((name,index)=>{
+                return <li key={index}>{name}</li>
+            })}<br/>
+
+            {subject.map(function(name,index){
+                return <li key={index}>{name}</li>
+            })}
+        </ul>
+
     );
 }
 
