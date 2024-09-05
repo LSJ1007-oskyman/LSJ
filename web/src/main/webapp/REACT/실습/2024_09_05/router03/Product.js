103aba5fcbf53ecf98281d35040bfb4c78d8b92d
LSJ1007-oskyman <pianoman1007@gmail.com>
2024-09-05 14:04:21 +0900

    Update Product.js

--- web/src/main/webapp/REACT/실습/2024_09_05/router03/Product.js
+++ web/src/main/webapp/REACT/실습/2024_09_05/router03/Product.js
@@ -1,13 +1,21 @@
 import React from "react";
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
-const Product=(props)=>{
-    const {productId} = useParams();
-    const param =useParams().productId;
-    return(
+const Product = (props) => {
+    const { productId } = useParams();
+    const location = useLocation();
+    return (
         <div>
             <h3>{productId}번 상품 페이지 입니다.(Product.js)</h3>
-            <h3>{param}번 상품 페이지 입니다.(Product.js)</h3>
+            <h3>useLocation</h3>
+            <ul>
+                <li>hash : {location.hash}</li>
+                <li>pathname : {location.pathname}</li>
+                <li>search : {location.search}</li>
+                <li>state : {location.state}</li>
+                <li>key : {location.key}</li>
+            </ul>
         </div>
     );
 }
