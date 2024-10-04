e5bb2ce87bcd1b98f04c7509bfbe2477a1d61e48
LSJ1007-oskyman <pianoman1007@gmail.com>
2024-10-04 16:54:40 +0900

    Delete web/src/main/webapp/jspproject/실습/2024_10_04/src/main directory

--- web/src/main/webapp/jspproject/실습/2024_10_04/src/main/java/service/Action.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-public interface Action {
-	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception;
-		
-}
